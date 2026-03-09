deef0511eb110123918ac13d395190cb4e856d72
Jhon Francia Minaya <jfranciaminaya@gmail.com>
2026-03-09 12:04:29 -0500

    WS para gestión de mascotas

---
+++ database.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE dbmascotas;
+USE dbmascotas;
+
+CREATE TABLE mascotas
+(
+  id      INT AUTO_INCREMENT PRIMARY KEY,
+  tipo    ENUM('Perro', 'Gato') NOT NULL,
+  nombre  VARCHAR(30)   NOT NULL,
+  color   VARCHAR(30)   NOT NULL,
+  pesokg  DECIMAL(5,2)  NOT NULL COMMENT '999.99'
+)ENGINE = INNODB;
+
+INSERT INTO mascotas (tipo, nombre, color, pesokg) VALUES
+  ('Perro', 'Firulais', 'blanco', 7),
+  ('Gato', 'Tony', 'negro', 3.2);
+
+SELECT * FROM mascotas LIMIT 5;
+
+UPDATE mascotas SET 
+  tipo = ?, 
+  nombre = ?, 
+  color = ?, 
+  pesokg = ? 
+WHERE id = ?
---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require('express')  //Framework
+const mysql = require('mysql2') //Acceso BD
+const bodyParser = require('body-parser') //Interactuar con JSON
+
+const app = express()
+app.use(bodyParser.json())
+
+//Configuración de acceso - .env
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'dbmascotas'
+})
+
+//Aperturar la conexión
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Conectado a la BD de mascotas")
+})
+
+//Iniciar el servidor
+const PORT = 3000
+
+// *** SERVICIOS WEB *** //
+//VERBO = ACCIÓN = INTENCIÓN
+//GET     : Leer 
+//POST    : Crear
+//PUT     : Actualizar todo
+//PATCH   : Actualización
+//DELETE  : Eliminar
+
+//req (require, requerimiento)
+//res (response, respuesta)
+app.post('/mascotas', (req, res) => {
+  //¿Y los datos que queremos guardar? - DESERIALIZACIÓN
+  const { tipo, nombre, color, pesokg } = req.body
+
+  //? = comodín, evita los ataques por SQLinjection
+  const sql = "INSERT INTO mascotas (tipo, nombre, color, pesokg) VALUES (?,?,?,?)"
+
+  db.query(sql, [tipo, nombre, color, pesokg], (err, results) => {
+    if (err) {
+      return res.status(500).send({ 
+        success: false, 
+        message: 'No se concretó el registro' 
+      })
+    } 
+    
+    //¿Qué hacemos cuando logramos registrar?
+    res.send({ 
+      success: true,
+      message: 'Nueva mascota registrada',
+      id: results.insertId
+    })
+  })
+})
+
+app.get('/mascotas', (req, res) => {
+  const sql = "SELECT * FROM mascotas LIMIT 10"
+  db.query(sql, (err, results) => {
+    if (err) return res.status(500).send({ message: 'Error acceso a datos'})
+    res.json(results)
+  })
+})
+
+//Se enviará el ID por la URL (endpoint)
+//Se enviarán los datos por JSON
+app.put('/mascotas/:id', (req, res) => {
+  const { id } = req.params
+  const { tipo, nombre, color, pesokg } = req.body
+
+  //Podemos escribir multilínea utilizando `
+  //COMODINES van en un índice
+  const sql = `
+  UPDATE mascotas SET 
+  tipo = ?, nombre = ?, color = ?, pesokg = ? 
+  WHERE id = ?`
+
+  db.query(sql, [tipo, nombre, color, pesokg, id], (err, results) => {
+    if (err) {
+      res.status(500).send({
+        success: false,
+        message: 'No se concretó la actualización'
+      })
+    }
+
+    return res.send({
+      success: true,
+      message: 'Registro actualizado'
+    })
+  })
+})
+
+//En un WS para eliminar un registro, pasamos la PK como parte de la ruta
+//Ejemplo: DELETE - miwebservice.com/clientes/379
+app.delete('/mascotas/:id', (req, res) => {
+  //¿De dónde obtenemos el ID a borrar? => ENDPOINT (URL)
+  const { id } = req.params
+  const sql = "DELETE FROM mascotas WHERE id = ?"
+  db.query(sql, [id] ,(err, results) => {
+    //Cláusula de GUARDA / retorno temprano
+    if (err){
+      res.status(500).send({
+        success: false,
+        message: 'No se puede eliminar el registro'
+      })
+    }
+
+    if (results.affectedRows == 0){
+      return res.status(404).send({
+        success: false,
+        message: 'No existe la mascota'
+      })
+    }
+
+    return res.send({
+      success: true,
+      message: 'Eliminado correctamente'
+    })
+    
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log("Servidor iniciado correctamente en http://localhost:3000")
+})
